4d789283d5a55c23453abc944f8d3f8084d0d043
Pavankumar07s <pawankumar14662693@gmail.com>
2024-09-28 23:56:50 +0530

    s3 completed at basic level

--- src/main/java/net/engineeringdigest/journalApp/Controllers/FaceComparisonController.java
+++ src/main/java/net/engineeringdigest/journalApp/Controllers/FaceComparisonController.java
@@ -1,54 +1,54 @@
-package net.engineeringdigest.journalApp.Controllers;
-import com.amazonaws.services.s3.AmazonS3;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import software.amazon.awssdk.services.rekognition.RekognitionClient;
-import software.amazon.awssdk.services.rekognition.model.*;
-
-
-@RestController
-@RequestMapping("/api/public/compare")
-public class FaceComparisonController {
-    private final RekognitionClient rekognitionClient;
-    private final String bucketName = "pavanprivateprac.dev";
-    private final AmazonS3 amazonS3;
-
-    public FaceComparisonController(RekognitionClient rekognitionClient, AmazonS3 amazonS3) {
-        this.rekognitionClient = rekognitionClient;
-        this.amazonS3 = amazonS3;
-
-    }
-
-    @GetMapping
-    public String compareFaces(@RequestParam String sourceImage, @RequestParam String targetImage) {
-        try {
-            Image source = Image.builder()
-                    .s3Object(S3Object.builder().bucket(bucketName).name(sourceImage).build())
-                    .build();
-
-            Image target = Image.builder()
-                    .s3Object(S3Object.builder().bucket(bucketName).name(targetImage).build())
-                    .build();
-
-            CompareFacesRequest request = CompareFacesRequest.builder()
-                    .sourceImage(source)
-                    .targetImage(target)
-                    .similarityThreshold(70F) // Adjust as per your requirements
-                    .build();
-
-            CompareFacesResponse response = rekognitionClient.compareFaces(request);
-
-            if (response.faceMatches().isEmpty()) {
-                return "No faces matched";
-            } else {
-                CompareFacesMatch match = response.faceMatches().getFirst();
-                return "Face match found with similarity: " + match.similarity();
-            }
-        } catch (RekognitionException e) {
-            e.printStackTrace();
-            return "Face comparison failed";
-        }
-    }
-}
+//package net.engineeringdigest.journalApp.Controllers;
+//import com.amazonaws.services.s3.AmazonS3;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//import software.amazon.awssdk.services.rekognition.RekognitionClient;
+//import software.amazon.awssdk.services.rekognition.model.*;
+//
+//
+//@RestController
+//@RequestMapping("/api/public/compare")
+//public class FaceComparisonController {
+//    private final RekognitionClient rekognitionClient;
+//    private final String bucketName = "pavanprivateprac.dev";
+//    private final AmazonS3 amazonS3;
+//
+//    public FaceComparisonController(RekognitionClient rekognitionClient, AmazonS3 amazonS3) {
+//        this.rekognitionClient = rekognitionClient;
+//        this.amazonS3 = amazonS3;
+//
+//    }
+//
+//    @GetMapping
+//    public String compareFaces(@RequestParam String sourceImage, @RequestParam String targetImage) {
+//        try {
+//            Image source = Image.builder()
+//                    .s3Object(S3Object.builder().bucket(bucketName).name(sourceImage).build())
+//                    .build();
+//
+//            Image target = Image.builder()
+//                    .s3Object(S3Object.builder().bucket(bucketName).name(targetImage).build())
+//                    .build();
+//
+//            CompareFacesRequest request = CompareFacesRequest.builder()
+//                    .sourceImage(source)
+//                    .targetImage(target)
+//                    .similarityThreshold(70F) // Adjust as per your requirements
+//                    .build();
+//
+//            CompareFacesResponse response = rekognitionClient.compareFaces(request);
+//
+//            if (response.faceMatches().isEmpty()) {
+//                return "No faces matched";
+//            } else {
+//                CompareFacesMatch match = response.faceMatches().getFirst();
+//                return "Face match found with similarity: " + match.similarity();
+//            }
+//        } catch (RekognitionException e) {
+//            e.printStackTrace();
+//            return "Face comparison failed";
+//        }
+//    }
+//}
--- src/main/java/net/engineeringdigest/journalApp/Controllers/S3Controller.java
+++ src/main/java/net/engineeringdigest/journalApp/Controllers/S3Controller.java
@@ -1,4 +1,6 @@
 package net.engineeringdigest.journalApp.Controllers;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import net.engineeringdigest.journalApp.Service.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,7 +16,7 @@ import java.nio.file.Files;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/public/files")
+@RequestMapping("/api/public/s3bucket")
 public class S3Controller {
 
     @Autowired
@@ -34,12 +36,26 @@ public class S3Controller {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("File upload failed: " + e.getMessage());
         }
     }
+    @PostMapping("/upload-to-folder")
+    public ResponseEntity<String> uploadFileToFolder(@RequestParam("file") MultipartFile file, String folder) {
+        try {
+            String fileName = s3Service.uploadFileToFolder(file,folder);
+            return ResponseEntity.status(HttpStatus.OK).body("File uploaded successfully: " + fileName);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("File upload failed: " + e.getMessage());
+        }
+    }
 
     // 2. Generate presigned URL for downloading
     @GetMapping("/download-url/{fileName}")
     public ResponseEntity<String> generateDownloadUrl(@PathVariable String fileName) {
         URL presignedUrl = s3Service.generatePresignedUrl(fileName);
         return ResponseEntity.status(HttpStatus.OK).body(presignedUrl.toString());
+    }
+    @GetMapping("/presigned-url")
+    public ResponseEntity<String> generatePreSignedUrl(@RequestParam("filePath") String filePath) {
+        String preSignedUrl = s3Service.generatePreSignedUrl(filePath);
+        return ResponseEntity.ok(preSignedUrl);
     }
 
     // 3. Download and stream file content from S3
@@ -62,6 +78,57 @@ public class S3Controller {
     public ResponseEntity<List<String>> listAllObjects() {
         List<String> objectKeys = s3Service.listAllObjects();
         return ResponseEntity.status(HttpStatus.OK).body(objectKeys);
+    }
+
+    // Delete a file
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteFile(@RequestParam("fileName") String fileName) {
+        s3Service.deleteFile(fileName);
+        return ResponseEntity.ok("File deleted successfully: " + fileName);
+    }
+
+    // Copy a file
+    @PostMapping("/copy")
+    public ResponseEntity<String> copyFile(@RequestParam("sourceKey") String sourceKey,@RequestParam("destinationKey") String destinationKey) {
+        s3Service.copyObject(sourceKey, destinationKey);
+        return ResponseEntity.ok("File copied successfully from " + sourceKey + " to " + destinationKey);
+    }
+
+    // Rename a file
+    @PostMapping("/rename")
+    public ResponseEntity<String> renameFile(@RequestParam("oldKey") String oldKey,
+                                             @RequestParam("newKey") String newKey) {
+        s3Service.renameFile(oldKey, newKey);
+        return ResponseEntity.ok("File renamed successfully from " + oldKey + " to " + newKey);
+    }
+
+    // Check if a file exists
+    @GetMapping("/exists")
+    public ResponseEntity<String> doesObjectExist(@RequestParam("fileName") String fileName) {
+        boolean exists = s3Service.doesObjectExist(fileName);
+        return exists ? ResponseEntity.ok("File exists: " + fileName)
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).body("File not found: " + fileName);
+    }
+
+    // Get metadata of a file
+    @GetMapping("/metadata")
+    public ResponseEntity<ObjectMetadata> getObjectMetadata(@RequestParam("fileName") String fileName) {
+        ObjectMetadata metadata = s3Service.getObjectMetadata(fileName);
+        return ResponseEntity.ok(metadata);
+    }
+
+    // List all buckets in the account
+    @GetMapping("/buckets")
+    public ResponseEntity<List<Bucket>> listAllBuckets() {
+        List<Bucket> buckets = s3Service.listAllBuckets();
+        return ResponseEntity.ok(buckets);
+    }
+
+    // List all files in a specific bucket (folder)
+    @GetMapping("/list-files")
+    public ResponseEntity<List<String>> listFilesInFolder(@RequestParam("folder") String folder) {
+        List<String> files = s3Service.listFilesInFolder(folder);
+        return ResponseEntity.ok(files);
     }
 
 
